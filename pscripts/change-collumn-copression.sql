8d5942e914298c5e0b3072d332b0c53bdf45d998
Alexey Lustin <alustin@silverbulleters.org>
2016-04-09 00:37:50 +0300

    исправлен комментарий в скрипте

--- pscripts/change-collumn-copression.sql
+++ pscripts/change-collumn-copression.sql
@@ -1,3 +1,6 @@
 ALTER TABLE <table-name> ALTER <column> SET STORAGE EXTENDED;
 
-#if the storage is main or extended, it means the data in given column is compressed (PostgreSQL uses very fast compression algorithm from the LZ family).
+-- if the storage is main or extended,
+-- it means the data in given column is compressed
+-- (PostgreSQL uses very fast compression algorithm from the LZ family).
+-- собственно PG сжимает данные, но не все и не всегда
